a3af33c20f27aa9bd72183cfa04ec0b764659a00
Nathan Hadzariga <n.hadzariga@gmail.com>
2015-06-22 02:00:09 -0300

    Fix tests for main controller.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -4,24 +4,28 @@ describe('Controller: MainCtrl', function() {
 
   // load the controller's module
   beforeEach(module('MainApp'));
-  var MainCtrl,$scope,$resource,_;
+  var MainCtrl, Nursery, $scope, _;
 
   // Initialize the controller and a mock scope
   beforeEach(function() {
-    angular.mock.inject(function ($injector) {
-      _ = $injector.get('lodash');
-      $scope = $injector.get('$rootScope').$new();
-      $resource = function() {};//$injector.get('$resource');
-      MainCtrl = $injector.get('MainCtrl', {
+    angular.mock.inject(function ($injector, $controller) {
+      // dependencies
+      Nursery = $injector.get('Nursery');
+      $scope = {};
+      // controller setup
+      MainCtrl = $controller('MainCtrl', {
         $scope: $scope,
-        $resource: $resource,
-        conf: {}
+        Nursery: Nursery
       });
+      // tools
+      _ = $injector.get('lodash');
     })
   });
 
-  it('should attach function getNurseries to scope', function() {
-    expect(_.isFunction($scope.getNurseries)).toBe(true);
+  describe('getNurseries', function() {
+    it('should be attached to scope', function() {
+      expect(_.isFunction($scope.getNurseries)).toBe(true);
+    });
   });
 
 });
